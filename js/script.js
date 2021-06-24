fc1ab4c2fa74a6df8fb8b1f4a0421da092474cb0
Luca Giozza <luca.giozza@gmail.com>
2021-06-24 14:53:26 +0200

    rinizio prova esercizio da 0

--- js/script.js
+++ js/script.js
@@ -1,96 +1,96 @@
 const icons = [
     {
-      name: 'cat',
+      nome: 'cat',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'crow',
+      nome: 'crow',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'dog',
+      nome: 'dog',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'dove',
+      nome: 'dove',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'dragon',
+      nome: 'dragon',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'horse',
+      nome: 'horse',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'hippo',
+      nome: 'hippo',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'fish',
+      nome: 'fish',
       prefix: 'fa-',
       type: 'animal',
       family: 'fas',
     },
     {
-      name: 'carrot',
+      nome: 'carrot',
       prefix: 'fa-',
       type: 'vegetable',
       family: 'fas',
     },
     {
-      name: 'apple-alt',
+      nome: 'apple-alt',
       prefix: 'fa-',
       type: 'vegetable',
       family: 'fas',
     },
     {
-      name: 'lemon',
+      nome: 'lemon',
       prefix: 'fa-',
       type: 'vegetable',
       family: 'fas',
     },
     {
-      name: 'pepper-hot',
+      nome: 'pepper-hot',
       prefix: 'fa-',
       type: 'vegetable',
       family: 'fas',
     },
     {
-      name: 'user-astronaut',
+      nome: 'user-astronaut',
       prefix: 'fa-',
       type: 'user',
       family: 'fas',
     },
     {
-      name: 'user-graduate',
+      nome: 'user-graduate',
       prefix: 'fa-',
       type: 'user',
       family: 'fas',
     },
     {
-      name: 'user-ninja',
+      nome: 'user-ninja',
       prefix: 'fa-',
       type: 'user',
       family: 'fas',
     },
     {
-      name: 'user-secret',
+      nome: 'user-secret',
       prefix: 'fa-',
       type: 'user',
       family: 'fas',
@@ -98,31 +98,26 @@ const icons = [
   ];
 
 
-//   Partendo dalla struttura dati , 
-//   mostriamo in pagina tutte le icone disponibili come da layout.
 
 
 
-print(icons);
-
-function print(array){
 
 
-icons.forEach((element) => {
-  const{name, prefix, type, family} = element;
-  const elementHTML = `
-  <div>
-                  <i   class="${family} ${prefix}${name} "></i>
-                  <p>${name.toUpperCase()}</p>
-              </div> 
-  
-              `;
+icons.forEach((element) =>{
+console.log(element);
 
-              document.getElementById('animali').innerHTML += elementHTML;
+
+const {nome, prefix, type, family} = element;
+const elementHtml =  `
+<div  class="contenitore">
+    <i  id="icone" class="fas fa-cat"></i>
+    <p>Cat</p> 
+ </div> 
+`;
+
+document.getElementById('icons').innerHTML += elementHtml ;
+
 });
-
-
-}
 
 
 
